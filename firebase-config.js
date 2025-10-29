a1ac918c7004a76cd5f06cec92112fcd2dd8f807
hr887 <hr@raystarllc.com>
2025-10-29 15:53:37 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+<!-- firebase-config.js -->
+<script>
+  window.firebaseConfig = {
+    apiKey: "YOUR_API_KEY",           // copy from your HR portal config
+    authDomain: "YOUR_AUTH_DOMAIN",   // copy from your HR portal config
+    projectId: "YOUR_PROJECT_ID",     // copy from your HR portal config
+    storageBucket: "YOUR_BUCKET"      // copy from your HR portal config
+  };
+</script>
